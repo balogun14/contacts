3a154b5c45719001e09ccef0e88c2b0930ec14b3
balogun14 <awwalbalogun06@gmail.com>
2022-12-17 23:33:18 +0100

    step1

---
+++ android/app/src/main/kotlin/awwal/pixolity/contacts/MainActivity.kt
@@ -0,0 +1,6 @@
+package awwal.pixolity.contacts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      routes: {
+        '/new-contact': (context) => const NewContactViewState(),
+      },
+    ),
+  );
+}
+
+class Contact {
+  final String name;
+
+  Contact({
+    required this.name,
+  });
+}
+
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  final List<Contact> _contacts = [];
+  int get lenght => _contacts.length;
+
+  void add({required Contact contact}) {
+    _contacts.add(contact);
+  }
+
+  void remove({required Contact contact}) {
+    _contacts.remove(contact);
+  }
+
+  Contact? contact({required int atIndex}) =>
+      _contacts.length > atIndex ? _contacts[atIndex] : null;
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final contactBook = ContactBook();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Home Page',
+            style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index)!;
+          return ListTile(
+            title: Text(contact.name),
+          );
+        },
+        itemCount: contactBook.lenght,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+          await Navigator.of(context).pushNamed('/new-contact');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewContactViewState extends StatefulWidget {
+  const NewContactViewState({super.key});
+
+  @override
+  State<NewContactViewState> createState() => _NewContactViewStateState();
+}
+
+class _NewContactViewStateState extends State<NewContactViewState> {
+  late final TextEditingController _controller;
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a new Contact'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: 'Enter a new contact name here...',
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              final contact = Contact(name: _controller.text);
+              ContactBook().add(contact: contact);
+              Navigator.of(context).pop();
+            },
+            child: const Text('Add Contact'),
+          )
+        ],
+      ),
+    );
+  }
+}
